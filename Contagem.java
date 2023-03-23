b6e4807acc8eed89a1d204acf480de2a40cd25a5
guikstro <116524405+guikstro@users.noreply.github.com>
2023-03-22 21:23:51 -0300

    Update Contagem.java

--- Contagem.java
+++ Contagem.java
@@ -1,13 +1,1 @@
-function contarInteiros(primeiroDado, n, conjuntoDeDados) {
-  let contador = 0;
-  for (let i = 0; i < conjuntoDeDados.length; i++) {
-    if (conjuntoDeDados[i] >= primeiroDado && conjuntoDeDados[i] <= n && Number.isInteger(conjuntoDeDados[i])) {
-      contador++;
-    }
-  }
-  return contador;
-}
-const primeiroDado = 5;
-const n = 12;
-const conjuntoDeDados = [5, 8, 7, 13, 11, 10, 6];
-document.write(contarInteiros(primeiroDado, n, conjuntoDeDados));
+
